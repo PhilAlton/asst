7408ab268df84da7b01e10dbb6352ae40ed1775f
Philip Alton <phil.alton@gmail.com>
2017-02-27 03:14:04 +0000

    fixed conflict resolution 2

--- asst/models.php
+++ asst/models.php
@@ -61,8 +61,7 @@ class User {
 		//Ensure user of UserName does not already exist
 		$query = New Query(SELECT, '* FROM `AuthTable` WHERE `UserName` =:UserName');
 		$conflict = $query->execute([':UserName' => $params['UserName']]);
-		if (isset($conflict)){
-			var_dump($conflict);
+		if (empty($conflict)){
 			echo "database conflict, user {$params['UserName']} alraedy exists";
 
 		} else {
