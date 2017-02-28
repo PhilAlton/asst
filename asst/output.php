32d82d4c46e0623fd3346b4a36ece07c5b8af764
Philip Alton <phil.alton@gmail.com>
2017-02-28 01:37:18 +0000

    fixed error log file

--- asst/output.php
+++ asst/output.php
@@ -34,7 +34,7 @@ class Output{
 		}
 
 
-        file_put_contents(realpath('/var/www/html/').'errorLog.txt', $errorLog);
+        file_put_contents(realpath('/var/www/html/').'error.txt', $errorLog);
 
 	}
 
