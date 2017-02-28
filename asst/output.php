72301c3387beb413a0841ba9c902ec187178c013
Philip Alton <phil.alton@gmail.com>
2017-02-28 03:00:37 +0000

    union

--- asst/output.php
+++ asst/output.php
@@ -38,6 +38,7 @@ class Output{
         $errorLog = $errorLog."</br><b>-------------------------------------------------------------------------</b></br></br></br></br>";
         echo FILE_APPEND;
         echo LOCK_EX;
+        echo FILE_APPEND | LOCK_EX
 
         file_put_contents(realpath('/var/www/html').'/error.html', $errorLog, FILE_APPEND | LOCK_EX);
 
