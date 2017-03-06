a6b6d539c5b0d27c02af5e5ea140eb7aab762f6e
PhilAlton <phil.alton@gmail.com>
2017-03-06 19:36:40 +0000

    ..

--- HelixTech/asstAPI/Output.php
+++ HelixTech/asstAPI/Output.php
@@ -58,9 +58,7 @@ class Output{
         }
 
 //commit these changes
-// other changes
-// which are great
-//greater
+
 
         // Construct error log header with connection details:
 		/** @todo rewrite connection to pull from / link against connection class? Output should only output when an error is present*/
