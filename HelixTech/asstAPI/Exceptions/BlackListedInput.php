2e01e890d18808c6015b3c20712a83a2f22fe015
Philip Alton <phil.alton@gmail.com>
2017-03-08 05:57:01 +0000

    .s

--- HelixTech/asstAPI/Exceptions/BlackListedInput.php
+++ HelixTech/asstAPI/Exceptions/BlackListedInput.php
@@ -13,7 +13,7 @@
            * Summary of BlackListedInput
            * @todo connect to the database to store log info for attempts to send blacklisted input to the server
            * This is a good indication of a attempted hack, and therefore a specific notification will be sent to
-           * the system administrator
+           * the system administrator, via slack.
            */
         public static function logError(){
             $message = "BlackList Attempt";
