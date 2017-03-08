deb1733b3ca6a75458003e964d84c07f5cd1eea9
Philip Alton <phil.alton@gmail.com>
2017-03-08 20:16:00 +0000

    test sending mail

--- HelixTech/Public/asstAPI/index.php
+++ HelixTech/Public/asstAPI/index.php
@@ -18,6 +18,17 @@ require_once dirname(dirname(__FILE__)) . '/../bootstrap.php';
 
 use HelixTech\asstAPI\{Connection, Router, Output};
 
+
+$to = 'phil.alton@gmail.com';
+$subject = 'Test PHP Mail';
+$message = 'Hello Phil';
+$headers = 'From: webmaster@axspa.org.uk' . "\r\n" .
+            'Reply-To: webmaster@axspa.org.uk' . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+
+
 Connection::connect();
 Router::route();
 Output::go();
