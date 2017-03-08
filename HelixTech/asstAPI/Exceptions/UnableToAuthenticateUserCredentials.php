15fcc4dc8fa51a1bed4933522733d2edf0297b32
Philip Alton <phil.alton@gmail.com>
2017-03-08 04:29:36 +0000

    fix 6

--- HelixTech/asstAPI/Exceptions/UnableToAuthenticateUserCredentials.php
+++ HelixTech/asstAPI/Exceptions/UnableToAuthenticateUserCredentials.php
@@ -15,7 +15,7 @@ class UnableToAuthenticateUserCredentials extends AbstractLoggedException
     public static function logError($message){
         $message = $message."...   ";
         $query = New Query(UPDATE, "ConnectionLog ".
-               "SET CXTN_ERRORS=:msg".
+               "SET CXTN_ERRORS=:msg ".
                "WHERE `CXTN_ID` =:cID");
         $query->execute([':cID' => Connection::getCID(), ':msg' => $message]);
     }
