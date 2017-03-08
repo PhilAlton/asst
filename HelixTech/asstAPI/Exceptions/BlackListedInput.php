a97cb6e2086e3b19bb29b2d66071745b3ed274fa
Philip Alton <phil.alton@gmail.com>
2017-03-08 04:19:48 +0000

    slack integration

--- HelixTech/asstAPI/Exceptions/BlackListedInput.php
+++ HelixTech/asstAPI/Exceptions/BlackListedInput.php
@@ -18,14 +18,28 @@
                    "SET CXTN_ERRORS=:msg".
                    "WHERE `CXTN_ID` =:cID");
             $query->execute([':cID' => Connection::$cID, ':msg' => $message]);
-        
 
 
-              // ALert system admin
-              // E.g Via slack
+
+            // ALert system admin
+            // E.g Via slack
+
+            // Instantiate with defaults, so all messages created
+            // will be sent from 'Cyril' and to the #accounting channel
+            // by default. Any names like @regan or #channel will also be linked.
+            $settings = [
+                'username' => 'asstapi',
+                'channel' => '#asstapi-log',
+                'link_names' => true
+            ];
+
+            $client = new Maknz\Slack\Client('https://hooks.slack.com/services/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', $settings);
+
+            $client->send('BlackListed word detected in HTTP input');
 
 
-          }
+
+        }
 
 
       }
