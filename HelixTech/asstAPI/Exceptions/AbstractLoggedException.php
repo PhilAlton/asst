ead39ebe8393d4d34dac4499f88ee9bf2867b99b
Philip Alton <phil.alton@gmail.com>
2017-03-08 04:47:10 +0000

    .

--- HelixTech/asstAPI/Exceptions/AbstractLoggedException.php
+++ HelixTech/asstAPI/Exceptions/AbstractLoggedException.php
@@ -16,7 +16,28 @@ abstract class AbstractLoggedException extends \Exception
 
 
     abstract public static function logError($message);
+    
+    public static function callSlack($message){
+        
+        // Instantiate with defaults, so all messages created
+        // will be sent from 'Cyril' and to the #accounting channel
+        // by default. Any names like @regan or #channel will also be linked.
+        $settings = [
+            'username' => 'asstapi',
+            'channel' => '#asstapi-log',
+            'link_names' => true
+        ];
+
+        $client = new Client('https://hooks.slack.com/services/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', $settings);
+
+        $client->send($message);
+
+
+    }
 
 }
+
+
+
 
 ?>
--- HelixTech/asstAPI/Exceptions/BlackListedInput.php
+++ HelixTech/asstAPI/Exceptions/BlackListedInput.php
@@ -23,23 +23,8 @@
             $query->execute([':cID' => Connection::getCID(), ':msg' => $message]);
 
 
-
-            // ALert system admin
-            // E.g Via slack
-
-            // Instantiate with defaults, so all messages created
-            // will be sent from 'Cyril' and to the #accounting channel
-            // by default. Any names like @regan or #channel will also be linked.
-            $settings = [
-                'username' => 'asstapi',
-                'channel' => '#asstapi-log',
-                'link_names' => true
-            ];
-
-            $client = new Client('https://hooks.slack.com/services/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', $settings);
-
-            $client->send('BlackListed word detected in HTTP input');
-
+            // ALert system admin via slack
+            parent::callSLack('BlackListed word detected in HTTP input');
 
 
         }
