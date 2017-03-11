5dd002dbc78b2e033805a03b156fad6a66bc037f
Philip Alton <phil.alton@gmail.com>
2017-03-11 16:04:29 +0000

    .

--- HelixTech/asstAPI/Crypt.php
+++ HelixTech/asstAPI/Crypt.php
@@ -82,8 +82,6 @@ class Crypt{
     public static function decrypt($input){
         $plaintext;
 
-        echo "isGen: ".$keyIsGeneral;
-
         try {
             // call Crypto::decrypt via UseEncryptionKey, mapping that functions argument syntax
             $plaintext = Crypt::UseEncryptionKey("Defuse\Crypto\Crypto::decrypt", $input, $key = null);
