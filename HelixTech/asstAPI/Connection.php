83fa5a6530d6fe19adc7e2db91a5d1a8d2255c26
Philip Alton <phil.alton@gmail.com>
2017-03-13 03:21:10 +0000

    debug 5

--- HelixTech/asstAPI/Connection.php
+++ HelixTech/asstAPI/Connection.php
@@ -58,7 +58,7 @@ class Connection{
             Connection::analyse($input);
             Connection::analyse(json_encode($_GET));
             $input = json_decode($input, true);
-
+            var_dump($input);
             Connection::$input = array();
             Connection::$input = !is_array($input) ? Connection::$input : array_merge(Connection::$input, $input);
             Connection::$input = !is_array($_GET) ? Connection::$input : array_merge(Connection::$input, $_GET);
