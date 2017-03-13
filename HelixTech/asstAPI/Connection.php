58a478601cd962e00fa87435e122b8834fbcc79d
Philip Alton <phil.alton@gmail.com>
2017-03-13 22:17:23 +0000

    .

--- HelixTech/asstAPI/Connection.php
+++ HelixTech/asstAPI/Connection.php
@@ -57,14 +57,10 @@ class Connection{
             $input = file_get_contents('php://input');
             Connection::analyse($input);
             Connection::analyse(json_encode($_GET));
-            var_dump(json_decode($input, true));
-            $input = json_decode($input, true);
-            var_dump($input);
-            Connection::$input = array();
-            Connection::$input = !is_array($input) ? Connection::$input : array_merge(Connection::$input, $input);
-            Connection::$input = !is_array($_GET) ? Connection::$input : array_merge(Connection::$input, $_GET);
 
-            var_dump(Connection::$input);
+            Connection::$input = array();
+            Connection::$input = !is_array($input) ? Connection::$input : array_merge(Connection::$input, json_decode($input, true));
+            Connection::$input = !is_array($_GET) ? Connection::$input : array_merge(Connection::$input, $_GET);
 
             // ensure connection via HTTPS
             if(!isset($_SERVER['HTTPS'])){
