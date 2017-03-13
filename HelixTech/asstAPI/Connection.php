082014e1d354052da3774a8d4879d54c3fd25e8c
Philip Alton <phil.alton@gmail.com>
2017-03-13 03:22:45 +0000

    .

--- HelixTech/asstAPI/Connection.php
+++ HelixTech/asstAPI/Connection.php
@@ -54,12 +54,11 @@ class Connection{
             Connection::$request = explode('/', trim($splitReqGet[0],'/'));
             Connection::$apiRoot = preg_replace('/[^a-z0-9_]+/i','',array_shift(Connection::$request));
 
-            echo $input = file_get_contents('php://input');
+            $input = file_get_contents('php://input');
             Connection::analyse($input);
             Connection::analyse(json_encode($_GET));
-            echo $input;
-            $input = json_decode($input, true);
-            var_dump($input);
+            $tother = json_decode($input, true);
+            var_dump($tother);
             Connection::$input = array();
             Connection::$input = !is_array($input) ? Connection::$input : array_merge(Connection::$input, $input);
             Connection::$input = !is_array($_GET) ? Connection::$input : array_merge(Connection::$input, $_GET);
