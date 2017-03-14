d5383665d2dd76efde8e066202d544e648947d8b
Philip Alton <phil.alton@gmail.com>
2017-03-14 01:18:04 +0000

    debug get 2

--- HelixTech/asstAPI/Models/Data.php
+++ HelixTech/asstAPI/Models/Data.php
@@ -149,7 +149,7 @@ class Data {
 
         // SQL query to return $data against date for User::uID
         foreach (Data::$userTableArray as $userTable){
-            $query = New Query(SELECT, "* from $userTable".User::$uID." WHERE LastUpdate > :remoteLastUpdate");
+            $query = New Query(SELECT, "* from $userTable".User::$uID." WHERE LastUpdate < :remoteLastUpdate");
             $results = array_merge($results, $query->execute([':remoteLastUpdate' => $remoteLastUpdate]));
         }
 
