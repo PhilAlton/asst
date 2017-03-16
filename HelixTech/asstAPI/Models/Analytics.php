8e7670448014b06199e8c6943192845b6b8f1fea
Philip Alton <phil.alton@gmail.com>
2017-03-16 00:43:58 +0000

    Added avrg. request to analytics

--- HelixTech/asstAPI/Models/Analytics.php
+++ HelixTech/asstAPI/Models/Analytics.php
@@ -9,17 +9,30 @@ class Analytics{
        try{
            if (Connection::authenticate('AdminTable')){
 
+           // build the data retrival queries
                $query = new Query(SELECT, "COUNT(DISTINCT CXTN_IP) FROM ConnectionLog");
                $numDistinctIP = $query->execute();
 
                $query = new Query(SELECT, "COUNT(DISTINCT CXTN_USER) FROM ConnectionLog");
                $numDistinctUsers = $query->execute();
 
+               $timePeriod = time() + (7 * 24 * 60 * 60);
+               $query = new Query(SELECT, "COUNT(*) FROM ConnectionLog WHERE UNIX_TIMESTAMP(CXTN_TIME) > $timePeriod");
+               $numAPIRequestsINlastWeekPerDay = ($query->execute())/7;
+
                $query = new Query(SELECT, "* FROM ConnectionLog WHERE CXTN_ERRORS IS NOT NULL ORDER BY CXTN_USER, CXTN_IP");
                $CnxtsByIP = $query->execute();
 
-               $analyticResults = array("DISTINCT_IP_COUNT" => $numDistinctIP,  "DISTINCT_USER_COUNT" => $numDistinctUsers, "Data" => $CnxtsByIP);
+
+
+            // combine the quries and output as JSON via Output class
+               $analyticResults = array(
+                                    "DISTINCT_IP_COUNT" => $numDistinctIP,  
+                                    "DISTINCT_USER_COUNT" => $numDistinctUsers, 
+                                    "AVERAGE_REQUESTS" => $numAPIRequestsINlastWeekPerDay,
+                                    "Data" => $CnxtsByIP);
                Output::setOutput($analyticResults);
+
 
 
             } else {
