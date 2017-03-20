0e325a6a41f130a402e267df1d58802095e8dfd7
Philip Alton <phil.alton@gmail.com>
2017-03-20 22:10:39 +0000

    .

--- HelixTech/asstAPI/Models/Data.php
+++ HelixTech/asstAPI/Models/Data.php
@@ -168,7 +168,7 @@ class Data {
                                     ." ORDER BY $firstTable.Date"
                                     );
 
-        $query = New Query (SELECT, "* from GEN_DATA_TABLE_100 FULL OUTER JOIN RCH_DATA_TABLE_100 ON GEN_DATA_TABLE_100.Date = RCH_DATA_TABLE_100.Date");
+        $query = New Query (SELECT, "* from GEN_DATA_TABLE_100, RCH_DATA_TABLE_100 ON GEN_DATA_TABLE_100.Date = RCH_DATA_TABLE_100.Date");
 
         $results = array_merge($results, $query->execute([':remoteLastUpdate' => $remoteLastUpdate]));
         
