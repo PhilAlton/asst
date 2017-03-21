69b4266cd307d3707f7ddb7df8cd2c4eb965698c
Philip Alton <phil.alton@gmail.com>
2017-03-21 00:10:37 +0000

    .

--- HelixTech/asstAPI/Models/Data.php
+++ HelixTech/asstAPI/Models/Data.php
@@ -159,7 +159,7 @@ class Data {
             } else {
                 $nextTable = $userTable.User::$uID;
                 $join = $join." RIGHT JOIN ".$nextTable." ON $firstTable.Date = $nextTable.Date";
-                $whereClause = $whereClause." AND WHERE UNIX_TIMESTAMP(".$nextTable.".LastUpdate) > :remoteLastUpdate";
+                $whereClause = $whereClause." AND UNIX_TIMESTAMP(".$nextTable.".LastUpdate) > :remoteLastUpdate";
             }
         }
         
@@ -170,8 +170,8 @@ class Data {
                                     ." ORDER BY $firstTable.Date"
         );
 
-   //     $query = New Query (SELECT, "* from GEN_DATA_TABLE_100 RIGHT JOIN RCH_DATA_TABLE_100 ON GEN_DATA_TABLE_100.Date = RCH_DATA_TABLE_100.Date WHERE UNIX_TIMESTAMP(GEN_DATA_TABLE_100.LastUpdate) > :remoteLastUpdate AND UNIX_TIMESTAMP(RCH_DATA_TABLE_100.LastUpdate) > :remoteLastUpdate");
-   //                          "SELECT * from GEN_DATA_TABLE_100 RIGHT JOIN RCH_DATA_TABLE_100 ON GEN_DATA_TABLE_100.Date = RCH_DATA_TABLE_100.Date WHERE UNIX_TIMESTAMP(GEN_DATA_TABLE_100) > :remoteLastUpdate AND WHERE UNIX_TIMESTAMP(RCH_DATA_TABLE_100) > :remoteLastUpdate ORDER BY GEN_DATA_TABLE_100.Date"
+        // $query = New Query (SELECT, "* from GEN_DATA_TABLE_100 RIGHT JOIN RCH_DATA_TABLE_100 ON GEN_DATA_TABLE_100.Date = RCH_DATA_TABLE_100.Date WHERE UNIX_TIMESTAMP(GEN_DATA_TABLE_100.LastUpdate) > :remoteLastUpdate AND UNIX_TIMESTAMP(RCH_DATA_TABLE_100.LastUpdate) > :remoteLastUpdate");
+        //                     "SELECT * from GEN_DATA_TABLE_100 RIGHT JOIN RCH_DATA_TABLE_100 ON GEN_DATA_TABLE_100.Date = RCH_DATA_TABLE_100.Date WHERE UNIX_TIMESTAMP(GEN_DATA_TABLE_100.LastUpdate) > :remoteLastUpdate AND UNIX_TIMESTAMP(RCH_DATA_TABLE_100.LastUpdate) > :remoteLastUpdate ORDER BY GEN_DATA_TABLE_100.Date"
         var_dump($query);
 
         $results = array_merge($results, $query->execute([':remoteLastUpdate' => $remoteLastUpdate]));
