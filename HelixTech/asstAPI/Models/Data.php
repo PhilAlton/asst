eb88b4318c4f28a6ce54b9fa4061203a3e12d04f
Philip Alton <phil.alton@gmail.com>
2017-03-26 23:06:28 +0100

    ,

--- HelixTech/asstAPI/Models/Data.php
+++ HelixTech/asstAPI/Models/Data.php
@@ -111,7 +111,7 @@ class Data {
                     $columnNames[] = $column['COLUMN_NAME'];
                     if (!isset($data[$column['COLUMN_NAME']])){
                         unset($columnNames[array_search($column['COLUMN_NAME'], $columnNames)]);
-                        if(count($columnNames)===1){Output::setOutput( implode(", ", $columnNames)); Output::errorMsg( implode(", ", $columnNames)); unset($columnNames['Date']); unset($columnNames['DataID']); unset($columnNames['LastUpdate']);}
+                        if(count($columnNames, COUNT_RECURSIVE)===1){Output::setOutput( implode(", ", $columnNames)); Output::errorMsg( implode(", ", $columnNames)); unset($columnNames['Date']); unset($columnNames['DataID']); unset($columnNames['LastUpdate']);}
                     } else {
                         $values[] = $data[$column['COLUMN_NAME']];
                     }
@@ -120,9 +120,9 @@ class Data {
 
                 // stringify columns and values
                 $columnString = implode(", ", $columnNames);
-                Output::errorMsg(count($columnString));
+                Output::errorMsg(count($columnString, COUNT_RECURSIVE));
                 Output::errorMsg($columnString);
-                Output::setOutput(count($columnString));
+                Output::setOutput(count($columnString, COUNT_RECURSIVE));
                 Output::setOutput($columnString);
                 $boundColumns = ":".implode(", :", $columnNames);
                 $boundValues = array_combine(explode(", ", $boundColumns) , $values);
