8a41b63629d84b4b5b1a69f23f31ae2d681d7fad
Philip Alton <phil.alton@gmail.com>
2017-03-26 23:08:05 +0100

    k

--- HelixTech/asstAPI/Models/Data.php
+++ HelixTech/asstAPI/Models/Data.php
@@ -111,7 +111,7 @@ class Data {
                     $columnNames[] = $column['COLUMN_NAME'];
                     if (!isset($data[$column['COLUMN_NAME']])){
                         unset($columnNames[array_search($column['COLUMN_NAME'], $columnNames)]);
-                        if(count($columnNames, COUNT_RECURSIVE)===1){Output::setOutput( implode(", ", $columnNames)); Output::errorMsg( implode(", ", $columnNames)); unset($columnNames['Date']); unset($columnNames['DataID']); unset($columnNames['LastUpdate']);}
+                        Output::setOutput( implode(", ", $columnNames)); Output::errorMsg( implode(", ", $columnNames)); 
                     } else {
                         $values[] = $data[$column['COLUMN_NAME']];
                     }
