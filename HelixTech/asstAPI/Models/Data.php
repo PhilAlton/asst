31f284f350cdd0d1603ce07412003b5b2d09170a
Philip Alton <phil.alton@gmail.com>
2017-03-26 21:22:18 +0100

    .

--- HelixTech/asstAPI/Models/Data.php
+++ HelixTech/asstAPI/Models/Data.php
@@ -111,7 +111,7 @@ class Data {
                     $columnNames[] = $column['COLUMN_NAME'];
                     if (!isset($data[$column['COLUMN_NAME']])){
                         unset($columnNames[array_search($column['COLUMN_NAME'], $columnNames)]);
-                        if(count($columnNames)==1){unset($columnNames['Date']);}
+                        if(count($columnNames)==3){unset($columnNames['Date']); unset($columnNames['DataID']); unset($columnNames['LastUpdate']);}
                     } else {
                         $values[] = $data[$column['COLUMN_NAME']];
                     }
