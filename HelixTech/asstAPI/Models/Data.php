018720e4f13743199e13c868ac66e400c16b82d9
Philip Alton <phil.alton@gmail.com>
2017-03-30 14:02:11 +0100

    .

--- HelixTech/asstAPI/Models/Data.php
+++ HelixTech/asstAPI/Models/Data.php
@@ -201,12 +201,12 @@ AND GEN_DATA_TABLE_100.DataID IS NULL
 ORDER BY GEN_DATA_TABLE_100.Date"*/
 
         $query = new Query(SELECT, 
-  //          "* from GEN_DATA_TABLE_100 "
-  //          ."RIGHT JOIN RCH_DATA_TABLE_100 ON GEN_DATA_TABLE_100.Date = RCH_DATA_TABLE_100.Date "
-  //          ."WHERE UNIX_TIMESTAMP(RCH_DATA_TABLE_100.LastUpdate) > :remoteLastUpdate"
+            "* from GEN_DATA_TABLE_100 "
+            ."RIGHT JOIN RCH_DATA_TABLE_100 ON GEN_DATA_TABLE_100.Date = RCH_DATA_TABLE_100.Date "
+            ."WHERE UNIX_TIMESTAMP(RCH_DATA_TABLE_100.LastUpdate) > :remoteLastUpdate"
   //          ."WHERE (UNIX_TIMESTAMP(RCH_DATA_TABLE_100.LastUpdate) > :remoteLastUpdate OR RCH_DATA_TABLE_100.LastUpdate IS NULL)"
-  //          ."UNION "
-  //          ."SELECT ". 
+            ."UNION "
+            ."SELECT ". 
             "* from RCH_DATA_TABLE_100 "
             ."RIGHT JOIN GEN_DATA_TABLE_100 ON RCH_DATA_TABLE_100.Date = GEN_DATA_TABLE_100.Date "
             ."WHERE UNIX_TIMESTAMP(GEN_DATA_TABLE_100.LastUpdate) > :remoteLastUpdate "
