0be7f2a5ed0c4304778fd43d19585894faf43c05
Philip Alton <phil.alton@gmail.com>
2017-03-30 13:52:47 +0100

    .

--- HelixTech/asstAPI/Models/Data.php
+++ HelixTech/asstAPI/Models/Data.php
@@ -206,9 +206,9 @@ ORDER BY GEN_DATA_TABLE_100.Date"*/
   //          ."WHERE UNIX_TIMESTAMP(GEN_DATA_TABLE_100.LastUpdate) > :remoteLastUpdate"
   //          ."WHERE (UNIX_TIMESTAMP(RCH_DATA_TABLE_100.LastUpdate) > :remoteLastUpdate OR RCH_DATA_TABLE_100.LastUpdate IS NULL)"
   //          ."UNION "
-  //          ."SELECT " 
+  //          ."SELECT ". 
             "* from RCH_DATA_TABLE_100 "
-            ."RIGHT JOIN GEN_DATA_TABLE_100 ON GEN_DATA_TABLE_100.Date = RCH_DATA_TABLE_100.Date "
+            ."RIGHT JOIN GEN_DATA_TABLE_100 ON RCH_DATA_TABLE_100.Date = GEN_DATA_TABLE_100.Date"
             ."WHERE UNIX_TIMESTAMP(RCH_DATA_TABLE_100.LastUpdate) > :remoteLastUpdate "
   //          ."AND UNIX_TIMESTAMP(RCH_DATA_TABLE_100.LastUpdate) > :remoteLastUpdate"
         );
