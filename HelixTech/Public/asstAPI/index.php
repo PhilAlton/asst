1d2a22adb46187c7ebda716467965f00f02ec8ca
Philip Alton <phil.alton@gmail.com>
2017-06-05 00:21:14 +0100

    perform whoIs look up on error'd connection

--- HelixTech/Public/asstAPI/index.php
+++ HelixTech/Public/asstAPI/index.php
@@ -18,27 +18,7 @@
 require_once dirname(dirname(__FILE__)) . '/../bootstrap.php';
 require_once 'analytics.php';
 
-
-$filename = 'https://who.is/whois-ip/ip-address/40.77.167.135';
-$whois = file_get_contents($filename);
-$start = strpos($whois, '<div class="col-md-12 queryResponseBodyKey"');
-$end = strpos($whois, '</pre>', $start);
-$whois = substr($whois, $start+49, $end-$start-49);
-$whois = explode("\n", $whois);
-$whoisAssoc = Array();
-foreach ($whois as $who){
-	$temp = explode(":",$who);
-	if (isset($temp[1])){
-		$whoisAssoc[$temp[0]] = $temp[1];
-	}
-}
-echo $whoisAssoc['NetName'].", ".$whoisAssoc['Organization'];
-
 use HelixTech\asstAPI\{Connection, Router, Output};
-
-
-
-
 
 Connection::connect();
 Router::route();
