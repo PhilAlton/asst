5f31d4e22a8d24079183ecf0c92ec3d488c0f27b
Philip Alton <phil.alton@gmail.com>
2017-06-04 23:08:16 +0100

    fread added

--- HelixTech/Public/asstAPI/index.php
+++ HelixTech/Public/asstAPI/index.php
@@ -18,7 +18,7 @@
 require_once dirname(dirname(__FILE__)) . '/../bootstrap.php';
 require_once 'analytics.php';
 $handle = fopen ('https://who.is/whois-ip/ip-address/40.77.167.135',"r");
-echo $handle;
+echo fread($handle,EOF);
 use HelixTech\asstAPI\{Connection, Router, Output};
 
 
