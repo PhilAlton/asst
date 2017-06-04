7a6a22fc47b934a8abf1502636d8bd83de6837f2
Philip Alton <phil.alton@gmail.com>
2017-06-04 23:35:44 +0100

    .

--- HelixTech/Public/asstAPI/index.php
+++ HelixTech/Public/asstAPI/index.php
@@ -20,9 +20,9 @@ require_once 'analytics.php';
 $filename = 'https://who.is/whois-ip/ip-address/40.77.167.135';
 $whois = file_get_contents($filename);
 
-echo $start = strpos($whois, '<div class="col-md-12 queryResponseBodyKey"');
-echo $end = strpos($whois, '</pre>', $start);
-echo substr($whois, $start+49, $end-$start-49);
+$start = strpos($whois, '<div class="col-md-12 queryResponseBodyKey"');
+$end = strpos($whois, '</pre>', $start);
+echo json_decode(substr($whois, $start+49, $end-$start-49),true);
 
 use HelixTech\asstAPI\{Connection, Router, Output};
 
