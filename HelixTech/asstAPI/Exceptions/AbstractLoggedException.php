55607ecf5a23e5524867870a527014b906bd7e2e
Philip Alton <phil.alton@gmail.com>
2017-06-05 00:32:54 +0100

    .

--- HelixTech/asstAPI/Exceptions/AbstractLoggedException.php
+++ HelixTech/asstAPI/Exceptions/AbstractLoggedException.php
@@ -42,7 +42,7 @@ abstract class AbstractLoggedException extends \Exception
 		if (isset($whoisAssoc['NetName']) and isset($whoisAssoc['Organization'])){
 			$whois = $whoisAssoc['NetName'].", ".$whoisAssoc['Organization'];
 		}
-        $query = New Query(UPDATE, "ConnectionLog SET CXTN_ERRORS=:msg, WHERE `CXTN_ID` =:cID");
+        $query = New Query(UPDATE, "ConnectionLog SET CXTN_ERRORS=:msg WHERE `CXTN_ID` =:cID");
         $query->silentExecute([':msg' => AbstractLoggedException::$dbMessage, ':cID' => Connection::getCID()]);
 
 		$query = New Query(UPDATE, "ConnectionLog SET CXTN_WHOIS=:whoIs WHERE `CXTN_ID` =:cID");
