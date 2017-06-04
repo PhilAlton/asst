ef65d52534e237596c1a8284878ee48a9ea3fb6d
Philip Alton <phil.alton@gmail.com>
2017-06-04 23:13:55 +0100

    .

--- HelixTech/Public/asstAPI/index.php
+++ HelixTech/Public/asstAPI/index.php
@@ -19,6 +19,7 @@ require_once dirname(dirname(__FILE__)) . '/../bootstrap.php';
 require_once 'analytics.php';
 $filename = 'https://who.is/whois-ip/ip-address/40.77.167.135';
 $handle = fopen ($filename,"r");
+echo filesize($filename);
 $contents = fread($handle,filesize($filename));
 fclose($filename);
 echo $contents;
