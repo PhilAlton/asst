0ef762ae3bede1328488ce801b0b1a92782fe4fe
Philip Alton <phil.alton@gmail.com>
2017-06-04 23:19:20 +0100

    .

--- HelixTech/Public/asstAPI/index.php
+++ HelixTech/Public/asstAPI/index.php
@@ -20,7 +20,7 @@ require_once 'analytics.php';
 $filename = 'https://who.is/whois-ip/ip-address/40.77.167.135';
 $whois = file_get_contents($filename);
 
-echo str_pos($whois, '<div class="col-md-12 queryResponseBodyKey"');
+echo strpos($whois, '<div class="col-md-12 queryResponseBodyKey"');
 
 
 use HelixTech\asstAPI\{Connection, Router, Output};
