de690a64c0cf92deba2420215a3a354c5cfd81bf
Philip Alton <phil.alton@gmail.com>
2017-06-04 23:32:09 +0100

    .

--- HelixTech/Public/asstAPI/index.php
+++ HelixTech/Public/asstAPI/index.php
@@ -22,7 +22,7 @@ $whois = file_get_contents($filename);
 
 echo $start = strpos($whois, '<div class="col-md-12 queryResponseBodyKey"');
 echo $end = strpos($whois, '</pre>', $start);
-echo substr($whois, $start+49, $end-$start+60);
+echo substr($whois, $start+49, $end-$start);
 
 use HelixTech\asstAPI\{Connection, Router, Output};
 
