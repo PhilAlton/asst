80a5a76866cbec50e3b17ff56ca1ffea3be1b18e
Philip Alton <phil.alton@gmail.com>
2017-06-15 21:00:16 +0100

    .

--- HelixTech/asstAPI/Exceptions/AbstractLoggedException.php
+++ HelixTech/asstAPI/Exceptions/AbstractLoggedException.php
@@ -29,63 +29,69 @@ abstract class AbstractLoggedException extends \Exception
 		// Resolve IP address into userful whois data
 		$whois = "";
 
-		// First look to who.is
-		// This should really point here! (to look up the managing reagion, and find the appropiate whois enpoint) : http://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.xhtm
-		$filename = 'https://who.is/whois-ip/ip-address/'.Connection::getIP();
-		$whois = file_get_contents($filename);
+		try { //because we're about to do a lot of unstable as hell site scrapping!
 
-		// Grab just the improtant data and load into an array
-		$start = strpos($whois, '<div class="col-md-12 queryResponseBodyKey"');
-		$end = strpos($whois, '</pre>', $start);
-		$whois = substr($whois, $start+49, $end-$start-49);
-		$whois = explode("\n", $whois);
-		var_dump($whois);
-		$whoisAssoc = Array();
-		foreach ($whois as $who){
-			$temp = explode(":",$who);
-			if (isset($temp[1])){
-				$whoisAssoc[$temp[0]] = $temp[1];
-			}
-		}		
-		
-		// If data is returned for Organization and NetName, then recored this
-		if (isset($whoisAssoc['NetName']) and isset($whoisAssoc['Organization'])){
-			$whois = $whoisAssoc['Organization'].", ".$whoisAssoc['NetName'];
-		} else {
-			$whoisAssoc['Organization'] = $whois[0];
-			$whoisAssoc['NetName'] = $whois[1];
-			$whois = $whoisAssoc['Organization'].", ".$whoisAssoc['NetName'];
-		}
-
-		// If returned data points to RIPE Network Coordination Centre (RIPE), 
-		//	then search the RIPE database for the whois data
-		if (strpos($whoisAssoc['Organization'], 'RIPE') !== false){
-
-		
-			$filename = 'https://apps.db.ripe.net/search/query.html?searchtext='.Connection::getIP().'#resultsAnchor';
+			// First look to who.is
+			// This should really point here! (to look up the managing reagion, and find the appropiate whois enpoint) : http://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.xhtm
+			$filename = 'https://who.is/whois-ip/ip-address/'.Connection::getIP();
 			$whois = file_get_contents($filename);
 
-			// If data is returned, then store this in the database
+			// Grab just the improtant data and load into an array
+			$start = strpos($whois, '<div class="col-md-12 queryResponseBodyKey"');
+			$end = strpos($whois, '</pre>', $start);
+			$whois = substr($whois, $start+49, $end-$start-49);
+			$whois = explode("\n", $whois);
+			var_dump($whois);
+			$whoisAssoc = Array();
+			foreach ($whois as $who){
+				$temp = explode(":",$who);
+				if (isset($temp[1])){
+					$whoisAssoc[$temp[0]] = $temp[1];
+				}
+			}		
+		
+			// If data is returned for Organization and NetName, then recored this
+			if (isset($whoisAssoc['NetName']) and isset($whoisAssoc['Organization'])){
+				$whois = $whoisAssoc['Organization'].", ".$whoisAssoc['NetName'];
+			} else {
+				$whoisAssoc['Organization'] = $whois[0];
+				$whoisAssoc['NetName'] = $whois[1];
+				$whois = $whoisAssoc['Organization'].", ".$whoisAssoc['NetName'];
+			}
 
-				// First isolate and grab the organisation name
-			$start = strpos($whois, 'Responsible organisation:');
-			$end = strpos($whois, '</a>', $start);
-			$whoisAssoc['Organization'] = substr($whois, $start+135, $end-$start-135);
+			// If returned data points to RIPE Network Coordination Centre (RIPE), 
+			//	then search the RIPE database for the whois data
+			if (strpos($whoisAssoc['Organization'], 'RIPE') !== false){
+
+		
+				$filename = 'https://apps.db.ripe.net/search/query.html?searchtext='.Connection::getIP().'#resultsAnchor';
+				$whois = file_get_contents($filename);
+
+				// If data is returned, then store this in the database
+
+					// First isolate and grab the organisation name
+				$start = strpos($whois, 'Responsible organisation:');
+				$end = strpos($whois, '</a>', $start);
+				$whoisAssoc['Organization'] = substr($whois, $start+135, $end-$start-135);
 			
-				// Then isolate and grab the NetName
-			$start = $end;
-			$end = strpos($whois, '</ul>', $start);
-			$whois = substr($whois, $start, $end-$start);
+					// Then isolate and grab the NetName
+				$start = $end;
+				$end = strpos($whois, '</ul>', $start);
+				$whois = substr($whois, $start, $end-$start);
 
-			$start = strpos($whois, 'netname:');
-			$end = strpos($whois, '</li>', $start);
-			$whoisAssoc['NetName'] = substr($whois, $start+17, $end-$start-17);
+				$start = strpos($whois, 'netname:');
+				$end = strpos($whois, '</li>', $start);
+				$whoisAssoc['NetName'] = substr($whois, $start+17, $end-$start-17);
 
-			// Combine the data to be incorporated into the slack message
-			$whois = $whoisAssoc['Organization'].", ".$whoisAssoc['NetName'];
+				// Combine the data to be incorporated into the slack message
+				$whois = $whoisAssoc['Organization'].", ".$whoisAssoc['NetName'];
 			
+			}
+		} catch (\Exception $e) {
+			
+			$whois = "Unable to Resolve IP Address: - error message: ".$e;
+
 		}
-
 
 
         $query = New Query(UPDATE, "ConnectionLog SET CXTN_ERRORS=:msg WHERE `CXTN_ID` =:cID");
