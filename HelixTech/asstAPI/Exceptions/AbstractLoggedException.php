e2e897d42ad13e62c7769cf07cfdbc69604b6da4
Philip Alton <phil.alton@gmail.com>
2017-06-15 20:20:41 +0100

    .

--- HelixTech/asstAPI/Exceptions/AbstractLoggedException.php
+++ HelixTech/asstAPI/Exceptions/AbstractLoggedException.php
@@ -72,7 +72,7 @@ abstract class AbstractLoggedException extends \Exception
 
 			$start = strpos($whois, 'netname:');
 			$end = strpos($whois, '</li>', $start);
-			$whoisAssoc['NetName'] = substr($whois, $start, $end-$start);
+			$whoisAssoc['NetName'] = substr($whois, $start+9, $end-$start-9);
 
 			var_dump(Array($start,$end,$whoisAssoc['Organization'],$whoisAssoc['NetName']));
 			
