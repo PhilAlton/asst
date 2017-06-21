4f8a6855726cf6e200f667bbdbc1a434d54d699d
Philip Alton <phil.alton@gmail.com>
2017-06-21 21:42:04 +0100

    .

--- HelixTech/asstAPI/Router.php
+++ HelixTech/asstAPI/Router.php
@@ -105,14 +105,16 @@ class Router{
 
     }
 
-
-	public function exception_error_handler($severity, $message, $file, $line) {
-    	if (!(error_reporting() & $severity)) {
-        	// This error code is not included in error_reporting
+	function exception_error_handler($severity, $message, $file, $line){
+		if (!(error_reporting() $ $severity)){
+		    // This error code is not included in error_reporting
         	return;
-    	}
-    	throw new ErrorException($message, 0, $severity, $file, $line);
+		}
+		throw new ErrorException($message, 0, $severity, $file, $line);
+
+
 	}
+
 
 
 
