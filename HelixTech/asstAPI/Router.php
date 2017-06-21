97919c5a91dd9c6fa55dbe1d9c5dc6b1e8c89215
Philip Alton <phil.alton@gmail.com>
2017-06-21 22:12:44 +0100

    .

--- HelixTech/asstAPI/Router.php
+++ HelixTech/asstAPI/Router.php
@@ -91,7 +91,7 @@ class Router{
         } catch (AttemptedToAccessUnauthorisedResources $e){
             Output::errorMsg("User details do not match requested resources");
         } catch (\Exception $e) {
-			http_response_code(404);
+			http_response_code(500);
 			Output::errorMsg("Other Error Thrown: ".$e->getMessage());
 			Output::setOutput('Error: '.$e->getMessage());
 		}
