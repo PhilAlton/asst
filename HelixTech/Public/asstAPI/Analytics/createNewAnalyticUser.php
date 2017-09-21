3a32c0679ef798f99db9d1667c07f52d4e928ed3
Philip Alton <phil.alton@gmail.com>
2017-09-21 22:38:19 +0100

    .

--- HelixTech/Public/asstAPI/Analytics/createNewAnalyticUser.php
+++ HelixTech/Public/asstAPI/Analytics/createNewAnalyticUser.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname(dirname(__FILE__)) . '/../bootstrap.php';
+require_once dirname(dirname(dirname(__FILE__))) . '/../bootstrap.php';
 
 use Defuse\Crypto\KeyProtectedByPassword;
 use Defuse\Crypto\Crypto;
