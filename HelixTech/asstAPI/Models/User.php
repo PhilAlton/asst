6a94c8887e6f4e86d94cac3bf921edec461d4a46
PhilAlton <phil.alton@gmail.com>
2017-09-24 19:08:06 +0100

    .

--- HelixTech/asstAPI/Models/User.php
+++ HelixTech/asstAPI/Models/User.php
@@ -639,8 +639,8 @@
 					// Send an email to the user containing the unique link
 					$message = 'Please click the following link to reset your password:' . "\r\n"
 								."https://axspa.org.uk/passwordReset.html?".urlencode("username=".$UserName."&GUIDE=".$uniqueCode) . "\r\n\r\n"
-								."debug: uniqueID=" . $uniqueID . "\r\n\r\n"
-								."debug: PassResTokEx=" . $expiary . "\r\n\r\n"
+						//		."debug: uniqueID=" . $uniqueID . "\r\n\r\n"
+						//		."debug: PassResTokEx=" . $expiary . "\r\n\r\n"
 								. "Please note, this link will expire in 12 hours";
 					
 					$headers = 'From: ResetPassword@axspa.org.uk' . "\r\n" .
