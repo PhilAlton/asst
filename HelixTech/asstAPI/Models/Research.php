a514c023b50bc8a653ecc5c4d129a4e7b3bad22b
Philip Alton <phil.alton@gmail.com>
2017-09-25 04:09:16 +0100

    .

--- HelixTech/asstAPI/Models/Research.php
+++ HelixTech/asstAPI/Models/Research.php
@@ -6,19 +6,44 @@ use HelixTech\asstAPI\Exceptions\UnableToAuthenticateUserCredentials;
 class Research{
 
     public static function display(){
-       try{
-           if (Connection::authenticate('AdminTable')){
+		try{
+			if (Connection::authenticate('AdminTable')){
 
-           // build the data retrival queries
+			// build the data retrival queries
 			$query = new Query(SELECT, "COUNT(DISTINCT UniqueID) FROM ResearchTable");
 			$numDistinctUsers = $query->execute(SIMPLIFY_QUERY_RESULTS_ON);
 
             $query = New Query(SELECT,
-							"Age, Gender, Age_Of_Symptom_Onset, Firstname, Surname, DoB, Hospital from UserTable"
+							"UniqueID, Age, Gender, Age_Of_Symptom_Onset, Firstname, Surname, DoB, Hospital from UserTable"
 							." RIGHT JOIN ResearchTable ON UserTable.UniqueID = ResearchTable.UniqueID"
 							." WHERE UserTable.Research_Participant = 1"      
 						);
             $researchParticipants = $query->execute(SIMPLIFY_QUERY_RESULTS_ON);
+
+			
+			
+			foreach($researchParticipants as &$researchParticipant){
+
+				$genTable = "GEN_DATA_TABLE_".$researchParticipant['UniqueID'];
+				$rchTable = "RCH_DATA_TABLE_".$researchParticipant['UniqueID'];
+
+				$query1 = New Query(SELECT,
+					"* from $genTable"
+					." RIGHT JOIN $rchTable ON $genTable.Date = $rchTable.Date"        
+				);
+
+				$query2 = New Query(SELECT,
+					"* from $rchTable"
+					." RIGHT JOIN $genTable ON $rchTable.Date = $genTable.Date"
+					." WHERE $rchTable.Date IS NULL"
+				);
+
+				$results = array_merge($results, $query1->execute(SIMPLIFY_QUERY_RESULTS_OFF,  [':remoteLastUpdate' => $remoteLastUpdate]));
+				$results = array_merge($results, $query2->execute(SIMPLIFY_QUERY_RESULTS_OFF,  [':remoteLastUpdate' => $remoteLastUpdate]));
+
+				$researchParticipant['data'] = $results;
+
+			}
 
 
 
