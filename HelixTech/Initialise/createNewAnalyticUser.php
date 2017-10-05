4f31b10ecb74d0d8ce6559168618d9694e660554
Philip Alton <phil.alton@gmail.com>
2017-10-05 20:38:25 +0100

    .

--- HelixTech/Initialise/createNewAnalyticUser.php
+++ HelixTech/Initialise/createNewAnalyticUser.php
@@ -7,7 +7,7 @@ use HelixTech\asstAPI\Query;
 
 
 $userName = "steven.zhao.25@gmail.com";
-$password = "axspa123"3567;
+$password = "axspa123"35678;
 
 
 $protected_key = KeyProtectedByPassword::createRandomPasswordProtectedKey($password);
