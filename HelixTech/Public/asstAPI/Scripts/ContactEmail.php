323082b202a0220e27bd6cbaae55295a49becd32
PhilAlton <phil.alton@gmail.com>
2017-12-26 21:25:25 +0000

    .

--- HelixTech/Public/asstAPI/Scripts/ContactEmail.php
+++ HelixTech/Public/asstAPI/Scripts/ContactEmail.php
@@ -1,21 +1,20 @@
 <?php
-echo "some other things";
-
-$webContactName = $_GET["webContactName"];
-$webContactEmail = $_GET["webContactEmail"];
-$webContactSubject = $_GET["webContactSubject"];
-$webContactMessage = $_GET["webContactMessage"];
+    $webContactName = $_GET["webContactName"];
+    $webContactEmail = $_GET["webContactEmail"];
+    $webContactSubject = $_GET["webContactSubject"];
+    $webContactMessage = $_GET["webContactMessage"];
 
 
-// Send an email to the user containing the unique link
-$message = "Message From: " . $webContactName . "(email: " . $webContactEmail . ")" . "\r\n\r\n"
-. "Subject: " . $webContactSubject . "\r\n"
-. $webContactMessage;
+    // Send an email to the user containing the unique link
+    $message = "Message From: " . $webContactName . "\r\n"
+    . "Email: " . $webContactEmail . "\r\n\r\n"
+    . "Subject: " . $webContactSubject . "\r\n"
+    . $webContactMessage;
 
-$headers = 'From: CRVW@axspa.org.uk' . "\r\n" .
-'Reply-To: '.$webContactEmail;
+    $headers = 'From: CRVW@axspa.org.uk' . "\r\n" .
+    'Reply-To: '.$webContactEmail;
 
-mail('inquire@helix-tech.co.uk', 'Ankylosing Spondylitis Symptom Tracker - Contact Request via Website', $message, $headers);
+    mail('inquire@helix-tech.co.uk', 'Ankylosing Spondylitis Symptom Tracker - Contact Request via Website', $message, $headers);
 
 ?>
 
