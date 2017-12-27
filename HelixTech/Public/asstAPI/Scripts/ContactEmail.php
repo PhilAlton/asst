465718224f56af58f65c7471a6d5aa53f26a33f1
PhilAlton <phil.alton@gmail.com>
2017-12-27 09:30:56 +0000

    .

--- HelixTech/Public/asstAPI/Scripts/ContactEmail.php
+++ HelixTech/Public/asstAPI/Scripts/ContactEmail.php
@@ -29,10 +29,19 @@
     .    "</style>"
     ."</head>"
     ."<body>"
-    ."<div style='max-width:500px;margin:auto;'>"
+    .    '<div class="w3-card-4 w3-hover-shadow w3-center" style="max-width:500px;margin:auto;">'
+    .    '<header class="w3-container w3-light-grey">'
     .    '<a href="#home" class="w3-bar-item w3-button w3-wide logo"><img src="/asst/HelixTech/Public/asstAPI/Images/logo.png" style="max-height:80px;max-width:450px;padding:0;margin:0;"></a>'
-    .    "<div><a href='https://testing.axspa.org.uk'>Email sent - we'll be in Touch!</a></div>"
-    ."<div>"
+    .    '</header>'
+    .    '<div class="w3-container">'
+    .    "<div class='w3-xxxlarge'><a href='https://testing.axspa.org.uk'>Email sent - we'll be in Touch!</a></div>"
+    .    '</div>'
+    .    '<button class="w3-button w3-block w3-dark-grey" onclick="document.getElementById(\'download\').style.display=\'block\'">Download <i class="fa fa-android"></i> <i class="fa fa-apple"></i> <!--<i class="fa fa-windows"></i>--></button>'
+    .    '</div>'
+ //   ."<div class='w3-card-4 w3-dark-grey' style='max-width:500px;margin:auto;'>"
+ //   .    '<a href="#home" class="w3-bar-item w3-button w3-wide logo"><img src="/asst/HelixTech/Public/asstAPI/Images/logo.png" style="max-height:80px;max-width:450px;padding:0;margin:0;"></a>'
+ //   .    "<div class='w3-xxxlarge'><a href='https://testing.axspa.org.uk'>Email sent - we'll be in Touch!</a></div>"
+ //   ."<div>"
     ."</body>"
     ."</html>";
 
