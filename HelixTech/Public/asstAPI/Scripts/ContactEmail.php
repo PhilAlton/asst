c393a564c24a1639296735e39ed6a328365e10aa
PhilAlton <phil.alton@gmail.com>
2017-12-27 09:18:42 +0000

    .

--- HelixTech/Public/asstAPI/Scripts/ContactEmail.php
+++ HelixTech/Public/asstAPI/Scripts/ContactEmail.php
@@ -21,10 +21,18 @@
     ."<html>"
     .    '<meta charset="UTF-8"/>'
     .    "<title>Contact Email</title>"
+    .    "<style>"
+    .        'body,h1,h2,h3,h4,h5 {font-family: "Poppins", sans-serif}'
+    .        "body {font-size: 16px;}"
+    .        "img {margin-bottom: -8px;}"
+    .        ".mySlides {display: none;}"
+    .    "</style>"
     ."</head>"
     ."<body>"
+    ."<div style='max-width:500px;margin:auto;'>"
     .    '<a href="#home" class="w3-bar-item w3-button w3-wide logo"><img src="/asst/HelixTech/Public/asstAPI/Images/logo.png" style="max-height:80px;max-width:450px;padding:0;margin:0;"></a>'
     .    "<div><a href='https://testing.axspa.org.uk'>Email sent - we'll be in Touch!</a></div>"
+    ."<div>"
     ."</body>"
     ."</html>";
 
