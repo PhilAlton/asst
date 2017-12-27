4dba2be8fa87815845d1a49511fb036552d3c9d4
PhilAlton <phil.alton@gmail.com>
2017-12-27 09:59:53 +0000

    .

--- HelixTech/Public/asstAPI/Scripts/ContactEmail.php
+++ HelixTech/Public/asstAPI/Scripts/ContactEmail.php
@@ -35,12 +35,12 @@
     ."</head>"
     
     ."<body>"
-    .    '<div class="w3-card-4 w3-hover-shadow w3-center" style="max-width:500px;margin:auto;">'
+    .    '<div class="w3-card-4 w3-hover-shadow w3-center" style="max-width:500px;margin:auto;margin-top:25%">'
     .    '<header class="w3-container w3-light-grey">'
     .    '<a href="#home" class="w3-bar-item w3-button w3-wide logo"><img src="/asst/HelixTech/Public/asstAPI/Images/logo.png" style="max-height:80px;max-width:450px;padding:0;margin:0;"></a>'
     .    '</header>'
     .    '<div class="w3-container">'
-    .    "<div class='w3-xxxlarge'><a href='https://testing.axspa.org.uk'>Email sent - we'll be in Touch!</a></div>"
+    .    "<div class='w3-xxxlarge'><a href='https://testing.axspa.org.uk'>Email sent - <br/>we'll be in Touch!</a></div>"
     .    '</div>'
     .    '<button class="w3-button w3-block w3-dark-grey" onclick="document.getElementById(\'download\').style.display=\'block\'">Download <i class="fa fa-android"></i> <i class="fa fa-apple"></i> <!--<i class="fa fa-windows"></i>--></button>'
     .    '</div>'
