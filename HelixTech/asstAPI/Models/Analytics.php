9c62eeaed4b1c9c410dba8fd6be32ee3a07cace6
PhilAlton <phil.alton@gmail.com>
2018-04-26 15:44:36 +0100

    .

--- HelixTech/asstAPI/Models/Analytics.php
+++ HelixTech/asstAPI/Models/Analytics.php
@@ -39,6 +39,89 @@ class Analytics{
                     array_push($cohortData, $query->execute(SIMPLIFY_QUERY_RESULTS_OFF));
                }
                
+               
+
+                $cohorts = array();
+                $firstCohort = 999999;
+                $lastCohort = 0;
+
+                foreach ($cohortData as $user){
+                   
+                    //asign the yyyymm of the earliest post ($user[?]["data"]) as $cohort
+                    $cohort = 999999;
+                    foreach ($user as $data){
+                        $date = substr($data["Date"],0,4).substr($data["Date"],5,2);
+                        if ((int)$date < $cohort){$cohort = (int)$date;}  
+                    }
+                    /*  orderedScores = sorted(score[1].keys())
+                        cohort = datetime.datetime.fromtimestamp(int(orderedScores[0])/1000).strftime('%Y%m')*/
+
+                    //if cohorts[cohort] is not set, then initialise 
+                    if (!isset($cohorts[$cohort])){
+                        $cohorts[$cohort] = Array("datapoints" => array(), "numofusers" => 0);
+                    }
+                  
+                    $cohorts[$cohort]["numofusers"]++;
+               
+                    foreach ($user as $data){
+                        $data = substr($data["Date"],0,4).substr($data["Date"],5,2);
+                        $cohorts[$cohort]["datapoints"][$data] = isset($cohorts[$cohort]["datapoints"][$data]) ? $cohorts[$cohort]["datapoints"][$data] + 1 : 1;
+                        
+                        // compare $data with first cohort and last cohort to asign these values
+                           if ((int)$data < $firstCohort){$firstCohort = (int)$data;} 
+                           if ((int)$data > $lastCohort){$lastCohort = (int)$data;} 
+                    }
+               }
+               
+
+               $diffYearsPart = int(substr($lastCohort,0,4))-int(substr($firstCohort,0,4));
+               $diffMonthsPart  = int(substr($lastCohort,5,2))-int(substr($firstCohort,5,2));
+               $diffMonths = ($diffYearsPart * 12) + $diffMonthsPart + 1;
+               
+               $cohortArray = array();
+               $i = 0;
+               $month = int(substr($firstCohort,5,2));
+               $year = int(substr($firstCohort,0,4));
+               while ($i < $diffMonths){
+                    while ($month <= 12){
+                        if ($month < 10){$month = "0".$month;}
+                        array_push($cohortArray, $year.$month);
+                        $month++;
+                        $i++;
+                        if ($i >= $diffMonths){break;}
+                    }
+                    $month = 1;
+                    $year++;
+               }
+                  
+               $cohortData = "";
+               foreach ($cohortArray as $cohort){
+                    $cohortData.= "</br>";
+                    $cohortData.= $cohort."- ";
+                    $i = 0;
+                    while ($i < $diffMonths){
+                        if (isset($cohorts[$cohort])){
+                            $cohortData.= ($i+1).": ";
+                            
+                            if (isset($cohorts[$cohort]["datapoints"][$cohortArray[$i]])){
+                                $cohortData.= ($cohorts[$cohort]["datapoints"][$cohortArray[i]]/$cohorts[$cohort]["numofusers"]);
+                            } else {
+                                $cohortData.= "0.00";
+                            }
+
+                            $i++;
+                        }
+                            
+                    }
+               }
+                   
+
+                   
+               
+
+
+
+
 
             // combine the quries and output as JSON via Output class
                $analyticResults = array(
